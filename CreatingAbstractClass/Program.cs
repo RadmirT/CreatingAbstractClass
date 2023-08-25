ed03bf9f0fe746a06f6e9c4325744d64766fd303
radmirt <radmirt@mail.ru>
2023-08-25 10:33:57 +0500

    Init

---
+++ CreatingAbstractClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+var type = typeof(Foo);
+var constructor = type.GetConstructor(Type.EmptyTypes); 
+var typeRuntimeMethodHandle = typeof(RuntimeMethodHandle);
+
+var invokeMethod = typeRuntimeMethodHandle.GetMethod("InvokeMethod", BindingFlags.Static | BindingFlags.NonPublic); 
+
+var signatureProperty = constructor.GetType().GetProperty("Signature", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance); 
+var abstractObject = (Foo)invokeMethod.Invoke(null, new object[] { null, null, signatureProperty.GetValue(constructor), true, null });
+
+abstractObject.Print();
+Console.WriteLine(abstractObject.GetType().ToString());
+
+public abstract class Foo
+{
+    public Foo()
+    {
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Я абстрактый класс");
+    }
+
+}
